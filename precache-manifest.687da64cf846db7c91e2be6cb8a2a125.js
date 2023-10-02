ae5f43c488dc2b322cb64c7f83436b3b34d70a4a
Manishjovi <manish4.85@gmail.com>
2023-10-02 13:49:16 +0530

    Updates

---
+++ precache-manifest.687da64cf846db7c91e2be6cb8a2a125.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3343c5ee34e6c638faa1",
+    "url": "/deploycart-react/static/css/main.6a66ca1f.chunk.css"
+  },
+  {
+    "revision": "3343c5ee34e6c638faa1",
+    "url": "/deploycart-react/static/js/main.8588812c.chunk.js"
+  },
+  {
+    "revision": "7da114c67c74bc79e5c2",
+    "url": "/deploycart-react/static/js/runtime~main.d92ace95.js"
+  },
+  {
+    "revision": "1496fc1b93e73fca0ef4",
+    "url": "/deploycart-react/static/css/2.05dd9737.chunk.css"
+  },
+  {
+    "revision": "1496fc1b93e73fca0ef4",
+    "url": "/deploycart-react/static/js/2.a8b7b71b.chunk.js"
+  },
+  {
+    "revision": "88d563725936ae2f2cac",
+    "url": "/deploycart-react/static/js/3.0e95e8df.chunk.js"
+  },
+  {
+    "revision": "21c319e7cd3dcc668c0b995242e80e80",
+    "url": "/deploycart-react/index.html"
+  }
+];
